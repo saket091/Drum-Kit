b410e37e9979cb2cb4d14933410db5dcf690dd18
saket091 <106917835+saket091@users.noreply.github.com>
2022-09-30 18:50:42 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const noOfButtons = document.querySelectorAll('.drum').length
+
+for(var i = 0; i < noOfButtons; i++){
+  document.querySelectorAll('.drum')[i].addEventListener('click', function(){
+    var audio = new Audio('sounds/tom-1.mp3')
+    audio.play()
+  });
+}
