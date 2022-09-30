6799f894bc4e4ec31205606951b30ac1407bd24e
saket091 <106917835+saket091@users.noreply.github.com>
2022-09-30 18:56:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const noOfButtons = document.querySelectorAll('.drum').length
 
 for(var i = 0; i < noOfButtons; i++){
   document.querySelectorAll('.drum')[i].addEventListener('click', function(){
-    var audio = new Audio('sounds/tom-1.mp3')
+    var audio = new Audio('tom-1.mp3')
     audio.play()
   });
 }
